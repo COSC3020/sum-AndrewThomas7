89bd90d8c58fb7abbe20af47f5d5226c57ceacb8
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-01-21 14:30:07 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,22 @@
+//COSC 3020
+//Andrew Thomas
+// Lars Kotthoff
+// 1/21/2025
+
+
+
 function sum(a) {
-    var sum = a[0];
+    var sum = 0;
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
 }
+
+
+// Unit tests
+// console.log(sum([3,2,1]));
+// console.log(sum([9,12,12]));
+// console.log(sum([36,35,36]));
+// console.log(sum([17,17,17]));
+// console.log(sum([13,13,13]));
